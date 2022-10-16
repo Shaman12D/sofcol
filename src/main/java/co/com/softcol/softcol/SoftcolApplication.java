1e3a3bc24a1e3f86e80d11c5add66678a66f3994
teamleaders2021 <parkista258@gmail.com>
2022-10-16 12:42:38 -0500

    Version 1.0

---
+++ src/main/java/co/com/softcol/softcol/SoftcolApplication.java
@@ -0,0 +1,13 @@
+package co.com.softcol.softcol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftcolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftcolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/softcol/softcol/SoftcolApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.softcol.softcol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftcolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
