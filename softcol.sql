c68bbdc61d83d2827200bb44490c5656492d0ce4
teamleaders2021 <equipo.leaders@gmail.com>
2022-10-28 22:39:53 -0500

    Version 2.0 (frontend agregado + funcionalidad ya implementada)

--- softcol.sql
+++ softcol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-10-2022 a las 10:52:30
+-- Tiempo de generación: 29-10-2022 a las 05:38:29
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -37,6 +37,16 @@ CREATE TABLE `administradores` (
   `Contraseña` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `administradores`
+--
+
+INSERT INTO `administradores` (`Id`, `Nombres`, `Apellidos`, `Teléfono`, `Dirección`, `Usuario`, `Contraseña`) VALUES
+(1, 'Carlos Armando', 'Devia', 3197820942, 'Mz H Casa 7', 'parkista258', 'xdxdxd'),
+(2, 'Carlos Eduardo', 'Casas', 3197820942, 'Mz H Casa 7', 'parkista258', 'xdxdxd'),
+(3, 'x', 'c', 2341845, 'fa 31 #54-65', 'asdasfas', 'gfghfdghgfh'),
+(4, 'Fabian Andrés', 'Devia Ochoa', 3197426520, 'Calle 31 #54-65', 'Fabian85747', '456152588');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +58,13 @@ CREATE TABLE `area` (
   `Área` varchar(50) NOT NULL,
   `Descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `area`
+--
+
+INSERT INTO `area` (`Id`, `Área`, `Descripcion`) VALUES
+(1, 'Vasectomía', 'Es el campo donde se practica.....');
 
 -- --------------------------------------------------------
 
@@ -83,6 +100,14 @@ CREATE TABLE `medicos` (
   `Contraseña` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `medicos`
+--
+
+INSERT INTO `medicos` (`Id`, `Nombres`, `Apellidos`, `FechaNacimiento`, `Teléfono`, `Dirección`, `Área`, `Usuario`, `Contraseña`) VALUES
+(1, 'dfsfds', 'dsad', '2000-02-24', 3197820911, 'hgfhhsgdf', 1, 'fdggfdhggf', 'dfgfdghgfdhgfh'),
+(2, 'x', 'cddf', '2000-03-24', 2341845, 'fafa 31 #54-65', 1, 'asdasfas', '4558945454');
+
 -- --------------------------------------------------------
 
 --
@@ -101,6 +126,43 @@ CREATE TABLE `pacientes` (
   `Usuario` varchar(100) NOT NULL,
   `Contraseña` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pacientes`
+--
+
+INSERT INTO `pacientes` (`Id`, `Cedula`, `Nombres`, `Apellidos`, `FechaNacimiento`, `Teléfono`, `Dirección`, `Estado`, `Usuario`, `Contraseña`) VALUES
+(1, 1001295983, 'Carlos Armando', 'Devia', '2000-02-27', 3197820942, 'Mz H Casa 7', 'activo', 'parkista258', 'xdxdxd'),
+(4, 1004798554, 'Carlos Eduardo', 'Casas', '2000-02-27', 3197820942, 'Mz H Casa 7', 'activo', 'parkista258', 'xdxdxd'),
+(5, 1005488, 'x', 'c', '1996-07-05', 2341845, 'fafa 31 #54-65', 'activo', 'asdasfas', '11111111'),
+(6, 132114475, 'Lizeth', 'Bermúdez', '1998-03-18', 5447812, 'Cra 97 #79-62', 'activo', 'Liz12A', '14547487562124');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vw_medicos_areas`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vw_medicos_areas` (
+`Id` int(11)
+,`Nombres` varchar(50)
+,`Apellidos` varchar(50)
+,`FechaNacimiento` date
+,`Teléfono` bigint(20)
+,`Dirección` varchar(100)
+,`Área` varchar(50)
+,`Usuario` varchar(50)
+,`Contraseña` varchar(100)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vw_medicos_areas`
+--
+DROP TABLE IF EXISTS `vw_medicos_areas`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_medicos_areas`  AS SELECT `medicos`.`Id` AS `Id`, `medicos`.`Nombres` AS `Nombres`, `medicos`.`Apellidos` AS `Apellidos`, `medicos`.`FechaNacimiento` AS `FechaNacimiento`, `medicos`.`Teléfono` AS `Teléfono`, `medicos`.`Dirección` AS `Dirección`, `area`.`Área` AS `Área`, `medicos`.`Usuario` AS `Usuario`, `medicos`.`Contraseña` AS `Contraseña` FROM (`medicos` join `area` on(`area`.`Id` = `medicos`.`Área`)) ;
 
 --
 -- Índices para tablas volcadas
@@ -149,13 +211,13 @@ ALTER TABLE `pacientes`
 -- AUTO_INCREMENT de la tabla `administradores`
 --
 ALTER TABLE `administradores`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `area`
 --
 ALTER TABLE `area`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `citas`
@@ -167,13 +229,13 @@ ALTER TABLE `citas`
 -- AUTO_INCREMENT de la tabla `medicos`
 --
 ALTER TABLE `medicos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
